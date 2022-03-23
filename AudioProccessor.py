781850d64bfd602326f7432b56c73e71d8eb6e41
Вадим Терентьев <vadimterentev@Vadims-MacBook-Air.local>
2022-03-23 20:08:34 +0300

    add weight for vectors

--- AudioProccessor.py
+++ AudioProccessor.py
@@ -24,7 +24,7 @@ class AudioProccessor(object):
                                       model=AudioProccessor.TORCH_MODEL_NAME)
 
         self.functions = {}
-        
+
         self.functions['get_speech_timestamps'] = utils[0]
         self.functions['read_audio'] = utils[2]
         self.model = model
@@ -41,27 +41,44 @@ class AudioProccessor(object):
         self.wav: Callable = self.functions['read_audio'](self.filename)
 
     def get_speech(self, seconds_flag: bool = True):
-        speech_timestamps = self.functions['get_speech_timestamps'](self.wav, self.model, return_seconds=seconds_flag)
-        return [(x['start'], x['end']) for x in speech_timestamps]
-        #return speech_timestamps
+        def weight_function(v):
+            return v[1] - v[0]
+        
+        speech_timestamps = self.functions['get_speech_timestamps'](
+            self.wav, self.model, return_seconds=seconds_flag)
+        noise_vector = [(x['start'], x['end']) for x in speech_timestamps]
+
+        noise_vector_with_weight = []
+        for v in noise_vector:
+            noise_vector_with_weight.append(
+                {'time': v, 'weight': weight_function(v)})
+
+        return noise_vector, noise_vector_with_weight
 
     def get_silent(self, noise_vector: list):
+        def weight_function(v):
+            return v[1] - v[0]
         silent_vector: list = []
 
         silent_vector.append((0, noise_vector[0][0]))
         for i in range(len(noise_vector)-1):
             silent_vector.append((noise_vector[i][1], noise_vector[i+1][0]))
         else:
-            silent_vector.append((noise_vector[-1][1], round(self.duration, 1)))
+            silent_vector.append(
+                (noise_vector[-1][1], round(self.duration, 1)))
 
-        return silent_vector
+        silent_vector_with_weight = []
+        for v in silent_vector:
+            silent_vector_with_weight.append(
+                {'time': v, 'weight': weight_function(v)})
 
+        return silent_vector, silent_vector_with_weight
 
 
 
 if __name__ == '__main__':
     A = AudioProccessor('../OSR_us_000_0010_8k.wav')
     A.read_file_wav()
-    h = A.get_silent(A.get_speech())
+    h = A.get_silent(A.get_speech()[0])
 
     f = 9
--- FileProccessor.py
+++ FileProccessor.py
@@ -3,18 +3,25 @@ import os
 import subprocess
 import typing
 import requests
-
-
+from urllib.parse import urlparse
 
 class FileProccessor(object):
 
-    TMP_PATH = '/tmp'
+    TMP_PATH: str = '/tmp'
+    FFMPEG_PARAM: str = '-y -i {} -vn -acodec pcm_s16le -ar 44100 -ac 1 {}' 
 
-    def __init__(self) -> None:
-        self._video_path = '/Users/vadimterentev/Downloads/BigBuckBunny.mp4'
-        self._ffmpeg_path = '/Users/vadimterentev/Downloads/ffmpeg'
-        self._ffmpeg_param = '-y -i {} -vn -acodec pcm_s16le -ar 44100 -ac 1 {}'
+    def __init__(self, file_http_path: str, ffmpeg_path: str) -> None:
+        if 'http' in file_http_path:
+            self._url = file_http_path
+            self._video_path = None
+        else:
+            self._url = None
+            self._video_path = file_http_path
 
+
+        self._ffmpeg_path = ffmpeg_path
+
+        self._ffmpeg_param = FileProccessor.FFMPEG_PARAM
         self._sound_dir = FileProccessor.TMP_PATH
 
 
@@ -32,14 +39,32 @@ class FileProccessor(object):
         
         return output_path
 
-    def get_video_from_url(self, url):
+    def get_video_from_url(self, url=None):
+        url = url or self._url
         res = requests.get(url, allow_redirects=True)
+
+        parsed_url = urlparse(url)
+        filename = os.path.basename(parsed_url.path)
+        self._video_path = os.path.join(FileProccessor.TMP_PATH, filename)
+
         with open(self._video_path, 'wb') as fb:
             fb.write(res.content)
 
+    def run(self, name):
+        # self.get_video_from_url()
+        self.get_audio_from_video()
+        
+
 if __name__ == '__main__':
-    A = FileProccessor()
+
+    fname = '/Users/vadimterentev/Downloads/BigBuckBunny.mp4'
+    ffmpeg = '/Users/vadimterentev/Downloads/ffmpeg'
+
+
+    
     s = '/Users/vadimterentev/Downloads/BigBuckBunny.mp4'
-    v = A.get_audio_from_video(s)
+    # v = A.get_audio_from_video(s)
     url = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
     #A.get_video_from_url(url)
+    A = FileProccessor('/tmp/BigBuckBunny.mp4', ffmpeg)
+    A.run(url)
