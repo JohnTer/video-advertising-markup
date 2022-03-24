ae8e2fc4e75980f6dfede90012d687ca48ed83f4
Вадим Терентьев <vadimterentev@Vadims-MacBook-Air.local>
2022-03-24 17:51:45 +0300

    Add VideoProccessor class

---
+++ VideoProcessor.py
@@ -0,0 +1,85 @@
+
+import math
+from tkinter.messagebox import NO
+
+import cv2
+import time, sys
+import imagehash
+
+from PIL import Image, ImageChops
+  
+# assign images
+#img1 = Image.open("1img.jpg")
+#img2 = Image.open("2img.jpg")
+#  
+## finding difference
+#diff = ImageChops.difference(img1, img2)
+#  
+## showing the difference
+#diff.show()
+
+
+
+
+class VideoProccessor(object):
+    def __init__(self, filename) -> None:
+        self.filename = filename
+        self.video_capture = None
+
+    def open_video(self):
+        self.video_capture = cv2.VideoCapture(self.filename)
+
+    def get_frame_hash(self, frame):
+        image = Image.fromarray(frame)
+        return imagehash.phash(image)
+
+    def get_frame_hashvector(self):
+        success = True
+        frame_hash_buffer = []
+
+        while success:
+            success, frame = self.video_capture.read()
+            frame_hash_buffer.append(self.get_frame_hash(frame))
+        return frame_hash_buffer
+
+    def get_frame_hashvector_diff(self):
+        success = True
+        frame_hash_buffer_diff = []
+
+        success, prev_frame = self.video_capture.read()
+        prev_frame_hash = self.get_frame_hash(prev_frame)
+        current_frame = None
+
+        frame_count = 0
+        while success:
+            success, current_frame = self.video_capture.read()
+            if not success:
+                break
+
+            current_frame_hash = self.get_frame_hash(current_frame)
+            frame_hash_buffer_diff.append(current_frame_hash - prev_frame_hash)
+            prev_frame_hash = current_frame_hash # Optimize it! store hash instead of frame
+
+            frame_count += 1
+        return frame_hash_buffer_diff
+
+
+    
+
+
+if __name__ == '__main__':
+    s = '/Users/vadimterentev/Downloads/output.mp4'
+    a = VideoProccessor(s)
+    a.open_video()
+    a.get_frame_hashvector_diff()
+
+    
+    
+
+
+
+
+
+
+
+
