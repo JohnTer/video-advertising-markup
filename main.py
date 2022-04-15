0ff7275aa00c0c420e04f353e09164e89adbec3d
Вадим Терентьев <vadimterentev@Vadims-MacBook-Air.local>
2022-04-15 15:42:34 +0300

    Add AdsMarkup class

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter.messagebox import NO
 from AudioProccessor import AudioProccessor
 from VideoProcessor import VideoProccessor
 from MarkupProccessor import MarkupProccessor
@@ -5,9 +6,10 @@ from FileProccessor import FileProccessor
 
 
 class AdsMarkup(object):
-    def __init__(self, filepath) -> None:
+    def __init__(self, filepath, ffmpeg_path = 'ffmpeg') -> None:
         self.filepath = filepath
-        self.ffmpeg_path = 'ffmpeg'
+        self.ffmpeg_path = ffmpeg_path
+        self.result = None
 
     def get_markup_vector(self):
         vp = VideoProccessor(s)
@@ -16,40 +18,32 @@ class AdsMarkup(object):
         vp_hashvector_score = MarkupProccessor().get_score(vp_hashvector_diff)
 
 
-        fp = FileProccessor(s, 'ffmpeg')
+        fp = FileProccessor(s, self.ffmpeg_path)
         audio = fp.get_audio_from_video()
-        AP = AudioProccessor(audio)
-        AP.read_file_wav()
-        silent = AP.get_silent(AP.get_speech()[0])[1]
+        ap = AudioProccessor(audio)
+        ap.read_file_wav()
+        silent_vector = ap.get_silent(ap.get_speech()[0])[1]
         
 
-        ll = MarkupProccessor().get_score(silent)
-        hh = MarkupProccessor().calculate_ads_score(q, ll)
-        hh.sort(key=lambda tup: tup['weight'], reverse=True)
+        silent_vector_score = MarkupProccessor().get_score(silent_vector)
+        
+        self.result = MarkupProccessor().calculate_ads_score(vp_hashvector_score, silent_vector_score)
+        self.result.sort(key=lambda d: d['weight'], reverse=True)
 
-    
-    
+        return self.result
+
+    def get_top_result(self, top=5):
+        return self.result[:top]
 
 
 if __name__ == '__main__':
 
-    s = '/home/john/Downloads/timati.mp4'
-    a = VideoProccessor(s)
-    a.open_video()
-    r = a.get_frame_hashvector_diff()
-    q = MarkupProccessor().get_score(r)
+    s = '/Users/vadimterentev/Downloads/output.mp4'
+    f = '/Users/vadimterentev/Downloads/ffmpeg'
 
-
-    fp = FileProccessor(s, 'ffmpeg')
-    audio = fp.get_audio_from_video()
-    AP = AudioProccessor(audio)
-    AP.read_file_wav()
-    silent = AP.get_silent(AP.get_speech()[0])[1]
-    
-
-    ll = MarkupProccessor().get_score(silent)
-    hh = MarkupProccessor().calculate_ads_score(q, ll)
-    hh.sort(key=lambda tup: tup['weight'], reverse=True)
+    a = AdsMarkup(s, f)
+    a.get_markup_vector()
+    v = a.get_top_result()
 
 
     g = 9
